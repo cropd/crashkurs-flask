78f47945dc3bc8acf3c4317512fd575319302e4f
Eduard Klinger <chillin@chillBook.local>
2010-11-02 13:16:23 +0100

    changed animation, added rooms, changed templates: base.html, room.html, list_rooms

--- crashkurs/models.py
+++ crashkurs/models.py
@@ -1,1 +1,5 @@
 from google.appengine.ext import db
+
+class Dicerolls(db.Model):
+	dice_roll = db.StringProperty(required=True)
+	room = db.StringProperty()
--- crashkurs/views.py
+++ crashkurs/views.py
@@ -1,17 +1,26 @@
 from crashkurs import app
 from flask import render_template, request, json
+from models import Dicerolls
 from random import randint
     
 @app.route('/')
 def start():
-    return render_template('start.html')
+    return render_template('list_rooms.html')
+    
+@app.route('/roll-dice/<room>')
+def show_room(room):
+	dicerolls = Dicerolls.all()
+	return render_template('room.html', dicerolls=dicerolls, room=room)
 
-@app.route('/roll-dice/<number>', methods=['GET'])
-def roll_dice(number):
+@app.route('/roll-dice/<room>/<number>', methods=['GET'])
+def roll_dice(room, number):
 	roll = [randint(1, 6)]
 	i = 1
 	while i < int(number):
 		roll.append(randint(1, 6))
 		i+=1	
-	dices ={"dices": roll}
-	return json.dumps(dices)
+	dice ={"dice": roll}
+	dicerolls = Dicerolls(dice_roll = str(dice), room = room)
+	dicerolls.put()
+	return json.dumps(dice)
+	
