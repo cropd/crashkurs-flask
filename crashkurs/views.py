a72b8ac9cd275a4f6cfff7a9aefd6d91035788cf
chillin <eduard.klinger@gmail.com>
2010-11-02 19:57:19 +0100

    linked rooms, added 404 error handler

--- crashkurs/views.py
+++ crashkurs/views.py
@@ -24,3 +24,7 @@ def roll_dice(room, number):
 	dicerolls.put()
 	return json.dumps(dice)
 	
+# Handle 404 errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
